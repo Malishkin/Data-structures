dfbfebe643333ac8e1081e7c7ad69f672588db06
SvetaLantsman <svetlana@curalife.com>
2023-05-26 19:05:44 +0300

    .

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ const japaneseRestaurant = {
   categories: ['Japanese', 'Sushi', 'Vegetarian', 'Organic'],
   appetizers: ['Seaweed salad', 'Tempura shrimp', 'Edamame', 'Sushi rice'],
   mainMenu: ['Sushi', 'Ramen', 'Tempura'],
-};
+  workingHorurs: {
+    wed: {
+      open: 10,
+      close: 23
+    },
+    fri: {
+      open: 10,
+      close: 23
+    },
+    sun: {
+      open: 12,
+      close: 23
+    }
+  },
+  orderFood: function (appetizerIndex, mainMenuIndex) { 
+  return `${this.appetizers[appetizerIndex]} and ${this.mainMenu[mainMenuIndex]}`
+},
 
-
-const array = [1, 2, [7, 9]];
-
-function getSeven(array) {
-  return array[2][0];
 }
+//Destructuring Objects
+function getRestaurantName(restaurant) {
+   return restaurant.name;  
+}
+//Destructuring Arrays
+// const array = [1, 2, [7, 9]];
 
-console.log(getSeven(array));
+// function getSeven(array) {
+//   return array[2][0];
+// }
+
+// function destructorizationArray(array) { 
+//   const [first,  , [third]] = array;
+//   return third;
+//  }
+
+// console.log(destructorizationArray(array));
+
+
